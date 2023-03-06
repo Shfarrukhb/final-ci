ce1433202258882e09012b98c95cb97aab534e5b
shfarrukb <shamsutdinov.f@gmail.com>
2023-03-06 13:00:51 +0500

    123

--- python/db_create.py
+++ python/db_create.py
@@ -4,7 +4,7 @@ import os
 dynamodb = boto3.client('dynamodb', region_name="us-east-1")
 table_name = os.getenv('db_table_name')
 
-def create_db(db_name)
+def create_db(db_name):
 
 
 dynamodb.create_table(
